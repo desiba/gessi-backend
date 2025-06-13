eb2088967d0362a09e8f6d14a2beb5e6d98ad33a
desiba <desiba_1@yahoo.com>
2025-06-13 06:27:23 +0100

    update pom

--- src/main/java/com/desmond/gadgetstore/GadgetStoreApplication.java
+++ src/main/java/com/desmond/gadgetstore/GadgetStoreApplication.java
@@ -2,12 +2,19 @@ package com.desmond.gadgetstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GadgetStoreApplication {
+public class GadgetStoreApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GadgetStoreApplication.class, args);
     }
+    
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GadgetStoreApplication.class);
+    }
+
 
 }
